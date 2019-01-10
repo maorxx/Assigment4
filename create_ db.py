1948263d87cd8c5638dc0ebf7797caa7eb99b935
maorelb@post.bgu.ac.il <gBwktp8x>
2019-01-10 18:32:59 +0200

    kjjkl

--- create_ db.py
+++ create_ db.py
@@ -15,7 +15,7 @@ def close_db():
     dbcon.commit()
     cursor.close()
     dbcon.close()
-    os.remove('schedule.db')
+    #os.remove('schedule.db')
 
 atexit.register(close_db)
 
@@ -25,7 +25,7 @@ def create_tables():
                                       class_id INTEGER REFERENCES classrooms(id),course_length INTEGER NOT NULL)""")
         cursor.execute("""CREATE TABLE students (gradeTEXT PRIMARY KEY , count INTEGER NOT NULL)""")
         cursor.execute(""" CREATE TABLE classrooms (ID INTEGER PRIMARY KEY , LOCATION TEXT NOT NULL , current_course_id INTEGER NOT NULL ,
-                                 current_course_time_lef INTEGER NOT NULL)""")
+                                 current_course_time_left INTEGER NOT NULL)""")
 
 def insert_course(id, course_name, student, number_of_students, class_id, course_length):
     cursor.execute("INSERT INTO courses VALUES (?, ?, ?, ?, ?, ?)", [id, course_name, student,number_of_students,class_id,course_length])
--- schedule.py
+++ schedule.py
@@ -12,8 +12,28 @@ dbcon = sqlite3.connect('schedule.db')
 cursor = dbcon.cursor()
 cursor.execute('SELECT * FROM ' + 'courses')
 lst = cursor.fetchall()
+num_of_courses=len(lst)
 
-for course in lst:
-    print(course)
-# while DBExist
+def get_course(class_id):
+    cursor.execute('SELECT courses.course_length from courses where courses.class_id =' + class_id)
+    course_length = cursor.fetchone()
+    return course_length[0]
+
+
+while DBExist and num_of_courses > 0:
+    #class room is free
+    cursor.execute('SELECT classrooms.id FROM classrooms where classrooms.current_course_time_left = 0')
+    free_classroom_ids = cursor.fetchall()
+    for row in free_classroom_ids:
+        course_length = get_course(str(row[0]))
+        cursor.execute('UPDATE classrooms set current_course_time_left =' + str(course_length) + 'where ID =' + str(row[0]))
+        # print(cursor.execute('SELECT classrooms.current_course_time FROM classrooms'))
+    break
+    #class room is occupied
+
+
+    #course just finished
+
+
+
 
